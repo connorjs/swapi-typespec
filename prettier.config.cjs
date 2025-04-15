109c4d4e31add6e7b9a1d469bff9eb637bd63879
Connor Sullivan <dev.connorjs@gmail.com>
2025-04-15 13:59:37 -0400

    ⬆️ Update non-tsp dependencies + reorganize (#5)
    Updates non-TypeSpec dependencies. Reorganizes the `package.json`, adds
    `.editorconfig`, switches to sentence-based line splitting for markdown,
    and updates the CI scripts including using `.node-version` file. Deletes
    no-longer desired PR template.

--- prettier.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	overrides: [{ files: "*.tsp", options: { parser: "typespec" } }],
-	plugins: ["@typespec/prettier-plugin-typespec"],
-	useTabs: true, // https://alexandersandberg.com/articles/default-to-tabs-instead-of-spaces-for-an-accessible-first-environment/
-};
